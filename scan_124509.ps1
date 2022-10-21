5c63ba9471f588e7bbaef6b290d7b1b4a5599ff5
tameval <114147355+tameval@users.noreply.github.com>
2022-10-21 09:56:09 +0900

    Update scan_124509.ps1

--- scan_124509.ps1
+++ scan_124509.ps1
@@ -1,5 +1,5 @@
 $tkn= Get-Content "C:\CRDemo\tkn.txt"
 $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}
-Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/scan_124509" -Headers $headers -OutFile $env:TEMP\scan.bat
+Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/scanning_dga.bat" -Headers $headers -OutFile $env:TEMP\scan.bat
 Start-Process -Wait $env:TEMP\scan.bat
 Remove-Item $env:TEMP\scan.bat
