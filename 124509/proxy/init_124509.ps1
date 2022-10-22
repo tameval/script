be560c3ff50ae02147301389715bca4d9ba19871
tameval <114147355+tameval@users.noreply.github.com>
2022-10-22 18:16:37 +0900

    Update init_124509.ps1

--- 124509/proxy/init_124509.ps1
+++ 124509/proxy/init_124509.ps1
@@ -3,7 +3,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $proxy_url = 'http://13.231.199.128:3128'; $web_client = New-Object System.Net.WebClient; $proxy_server = New-Object System.Net.WebProxy($proxy_url, $true); $web_client.Proxy = $proxy_server
 
 #7zip
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/unzip_124509.ps1')
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/unzip_124509pr.ps1')
 
 #scanning
 IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan_124509.ps1')
