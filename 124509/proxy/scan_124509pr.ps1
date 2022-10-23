314c0813874991393665ccfd639ea95d9da6c210
tameval <114147355+tameval@users.noreply.github.com>
2022-10-24 00:01:51 +0900

    Update scan_124509pr.ps1

--- 124509/proxy/scan_124509pr.ps1
+++ 124509/proxy/scan_124509pr.ps1
@@ -1,4 +1,4 @@
 $proxy_url = Get-Content 'C:\CRDemo\proxy.txt' -ErrorAction SilentlyContinue
 Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan.bat" -Proxy $proxy_url -OutFile $env:TEMP\scan.bat
-Start-Process -Wait $env:TEMP\scan.bat $proxy_url
+Start-Process -Wait $env:TEMP\scan.bat
 Remove-Item $env:TEMP\scan.bat
