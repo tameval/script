c2db4e4caaebc2b9720b60ec62d13ad40fa2e7c9
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 17:01:36 +0900

    Create 7zip_33667.ps1

---
+++ 33667/7zip_33667.ps1
@@ -0,0 +1,6 @@
+if ((Get-Item -Path 'C:\Program Files\7-Zip\7z.exe' -eq $null)
+{
+  C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://7-zip.org/a/7z2201-x64.exe $env:TEMP\7z.Exe
+  Start-Process -FilePath $env:TEMP\7z.Exe -Args "/S" -Verb RunAs -Wait
+  Remove-Item $env:TEMP\7z.Exe
+}
