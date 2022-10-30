58bc7dfdc2ed5ebcc6c05a5a944b738c62f90e33
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 18:26:18 +0900

    Create hosts_33667pr.ps1

---
+++ 33667/proxy/hosts_33667pr.ps1
@@ -0,0 +1,3 @@
+cmd.exe /c "echo 1.1.1.1 www.hoge111.local #this is test1 >> C:\Windows\System32\drivers\etc\hosts"
+cmd.exe /c "echo 2.2.2.2 www.hoge222.local #this is test2 >> C:\Windows\System32\drivers\etc\hosts"
+cmd.exe /c "echo 3.3.3.3 www.hoge333.local #this is test3 >> C:\Windows\System32\drivers\etc\hosts"
